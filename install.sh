8c5959c1c33e91e624ce0d0cc23eb83df65e84e0
josewdf <60181010+josemlwdf@users.noreply.github.com>
2025-04-16 16:34:09 +0200

    Create install.sh
    created installer script

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo pip install scapy --break-system-packages
+cd /opt; sudo wget https://raw.githubusercontent.com/josemlwdf/PCAP_Parser/refs/heads/main/pcap_parser.py
+sudo ln -s /opt/pcap_parser.py /usr/sbin/pcap_parser
