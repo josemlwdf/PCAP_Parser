07856c37adbf5ea4131b9bdc67ce501edc6466cd
josewdf <60181010+josemlwdf@users.noreply.github.com>
2025-04-16 16:36:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo pip install scapy --break-system-packages
 cd /opt; sudo wget https://raw.githubusercontent.com/josemlwdf/PCAP_Parser/refs/heads/main/pcap_parser.py
-chmod +x /opt/pcap_parser.py
+sudo chmod +x /opt/pcap_parser.py
 sudo ln -s /opt/pcap_parser.py /usr/sbin/pcap_parser
